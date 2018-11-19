43fa36192892c5d6801b5e43f70ecb549608f843
githubshenyuan <35088383+githubshenyuan@users.noreply.github.com>
2018-11-19 09:46:30 +0800

    oauth2

---
+++ springboot-oauth2-demo/src/main/java/com/example/springbootoauth2demo/SpringbootOauth2DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2DemoApplication.class, args);
+    }
+}
